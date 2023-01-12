eeee5721aef64ee60e3cdec0275f4cfebe2c0036
Heberth Alexandre <121596722+juliaandreds@users.noreply.github.com>
2023-01-11 22:21:47 -0300

    Limpando o formulário

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -13,6 +13,8 @@ export class NegociacaoController {
     adiciona(): void{
         const negociacao = this.criaNegociacao();
         console.log(negociacao);
+
+        this.limparFormulario();
     }
     
     criaNegociacao(): Negociacao {
@@ -22,5 +24,12 @@ export class NegociacaoController {
         const valor = parseInt(this.inputValor.value);
     
         return new Negociacao(date, quantidade, valor);
+    }
+
+    limparFormulario(): void {
+        this.inputData.value = ''; 
+        this.inputQuantidade.value = ''; 
+        this.inputValor.value = '';
+        this.inputData.focus();
     }
 }
