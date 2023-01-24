beb30d87af913f44417c2be977b9f3325f465023
Heberth Alexandre <121596722+juliaandreds@users.noreply.github.com>
2023-01-24 20:50:44 -0300

    Criando nossa primeira view

--- dist/js/controllers/negociacao-controller.js
+++ dist/js/controllers/negociacao-controller.js
@@ -7,24 +7,31 @@ export class NegociacaoController {
         this.inputQuantidade = document.querySelector('#quantidade');
         this.inputValor = document.querySelector('#valor');
     }
+
     adiciona() {
         const negociacao = this.criaNegociacao();
+
         this.negociacoes.adiciona(negociacao);
         negociacao.data.setDate(12);
+
         console.log(this.negociacoes.lista());
         this.limparFormulario();
     }
+
     criaNegociacao() {
         const exp = /-/g;
         const date = new Date(this.inputData.value.replace(exp, ','));
         const quantidade = parseInt(this.inputQuantidade.value);
         const valor = parseInt(this.inputValor.value);
+
         return new Negociacao(date, quantidade, valor);
     }
+
     limparFormulario() {
         this.inputData.value = '';
         this.inputQuantidade.value = '';
         this.inputValor.value = '';
+        
         this.inputData.focus();
     }
 }
--- dist/js/models/negociacoes.js
+++ dist/js/models/negociacoes.js
@@ -2,9 +2,11 @@ export class Negociacoes {
     constructor() {
         this.negociacoes = [];
     }
+
     adiciona(negociacao) {
         this.negociacoes.push(negociacao);
     }
+    
     lista() {
         return this.negociacoes;
     }
